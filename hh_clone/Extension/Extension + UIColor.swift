42288b774d994047cbdf2cc60834113be5cf3769
Halil Yavuz <halil9913@gmail.com>
2024-09-27 17:17:38 +0300

    added color+

---
+++ hh_clone/Extension/Extension + UIColor.swift
@@ -0,0 +1,19 @@
+//
+//  Extension + UIColor.swift
+//  hh_clone
+//
+//  Created by Halil Yavuz on 27.09.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let loginInputBackground = UIColor(named: "loginInputView")
+    let loginInputTF = UIColor(named: "loginInputTF")
+    let continueButton = UIColor(named: "continueButton")
+    let findEmployeeButton = UIColor(named: "findEmployeeButton")
+}
