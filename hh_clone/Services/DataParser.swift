23004616214a8ae9a4a46d266b50f25980d2fd9c
Halil Yavuz <halil9913@gmail.com>
2024-10-18 14:26:47 +0300

    fixed DataParaser

--- hh_clone/Services/DataParser.swift
+++ hh_clone/Services/DataParser.swift
@@ -9,8 +9,8 @@ import Foundation
 
 final class DataParser {
     
-    func fetchVacancies(completion: @escaping (Result<[Vacancy], Error>) -> Void) {
-        guard let jsonFilePath = Bundle.main.path(forResource: "vacancies", ofType: "json") else {
+    func fetchVacancies(completion: @escaping (Result<Vacancy, Error>) -> Void) {
+        guard let jsonFilePath = Bundle.main.path(forResource: "json", ofType: "json") else {
             print("Failed to locate vacancies JSON file")
             return
         }
@@ -19,24 +19,25 @@ final class DataParser {
         do {
             let data = try Data(contentsOf: fileURL)
             let decoder = JSONDecoder()
-            let vacancies = try decoder.decode([Vacancy].self, from: data)
+            let vacancies = try decoder.decode(Vacancy.self, from: data)
             completion(.success(vacancies))
         } catch {
             completion(.failure(error))
         }
     }
     
-    func fetchOffers(completion: @escaping (Result<[Offers], Error>) -> Void) {
-        guard let jsonFilePath = Bundle.main.path(forResource: "offers", ofType: "json") else {
+    func fetchOffers(completion: @escaping (Result<Offers, Error>) -> Void) {
+        guard let jsonFilePath = Bundle.main.path(forResource: "json", ofType: "json") else {
             print("Failed to locate offers JSON file")
             return
         }
         
         let fileURL = URL(fileURLWithPath: jsonFilePath)
         do {
+            
             let data = try Data(contentsOf: fileURL)
             let decoder = JSONDecoder()
-            let offers = try decoder.decode([Offers].self, from: data)
+            let offers = try decoder.decode(Offers.self, from: data)
             completion(.success(offers))
         } catch {
             completion(.failure(error))
